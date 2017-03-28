36db9c668d361fbd3cef77e69a80b7f065d83333
Sundarina <sundarina@ukr.net>
2017-03-28 13:13:08 +0300

    чат-клиент

---
+++ src/main/Const.java
@@ -0,0 +1,5 @@
+package main;
+
+public abstract class Const {
+    public static final int Port = 8283;
+}
---
+++ src/main/MainClass.java
@@ -0,0 +1,28 @@
+package main;
+
+import client.Client;
+import server.Server;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Запустить программу в режиме сервера или клиента? (S(erver) / C(lient))");
+        while (true) {
+            char answer = Character.toLowerCase(in.nextLine().charAt(0));
+            if (answer == 's') {
+                new Server();
+                break;
+            } else if (answer == 'c') {
+                new Client();
+                break;
+            } else {
+                System.out.println("Некорректный ввод. Повторите.");
+            }
+        }
+    }
+}
